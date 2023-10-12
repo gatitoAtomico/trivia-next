0d181fb499815a99105d043fbf687dca5142159f
gatitoAtomico <sup.georgios@gmail.com>
2023-10-12 15:13:39 +0300

    update project, use of style components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #bf4f74;
+  `;
+
+  return <Title>Hello World!</Title>;
 }
 
 export default App;
